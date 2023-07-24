a46bb49f825af0a0547319718d7974da1fc697d6
Ankit Kapoor <ankitkapoor8437@gmail.com>
2023-07-24 11:11:46 +0530

    Completed navigation section with functionality.

---
+++ src/assets/data/navigation.js
@@ -0,0 +1,20 @@
+const navlinks = [
+    {
+        path: '/home',
+        display: 'Home'
+    },
+    {
+        path: '/doctors',
+        display: 'Find a Doctor'
+    },
+    {
+        path: '/services',
+        display: 'Services'
+    },
+    {
+        path: '/contact',
+        display: 'Contact'
+    },
+]
+
+export default navlinks;
